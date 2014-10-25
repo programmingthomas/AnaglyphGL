623ada716ac1cd2e12a7abda6a8848c568bc2730
Programming Thomas <programmingthomas@gmail.com>
2014-10-25 06:06:16 +0100

    Offscreen Buffer creation and management

---
+++ Mountains IIb/Mountains IIb/OffscreenBuffer.mm
@@ -0,0 +1,63 @@
+// OffscreenBuffer.m
+//
+// Copyright 2014 Thomas Denney
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+
+#import "OffscreenBuffer.h"
+
+OffscreenBuffer::OffscreenBuffer(GLsizei width, GLsizei height) {
+    this->width = width;
+    this->height = height;
+    
+    //1. Generate texture, framebuffer and depthbuffer
+    //The texture is what gets rendered into offscreen
+    
+    glGenTextures(1, &texture);
+    glGenFramebuffers(1, &frameBuffer);
+    glGenRenderbuffers(1, &depthBuffer);
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+    
+    //2. Bind and configure the texture
+    
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+    
+    //3. Create and attach the depth buffer
+    
+    glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer);
+    
+    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (status != GL_FRAMEBUFFER_COMPLETE) {
+        std::cout << "Framebuffer is not complete " << status <<  std::endl;
+    }
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+OffscreenBuffer::~OffscreenBuffer() {
+    glDeleteRenderbuffers(1, &depthBuffer);
+    glDeleteFramebuffers(1, &frameBuffer);
+    glDeleteTextures(1, &texture);
+}
